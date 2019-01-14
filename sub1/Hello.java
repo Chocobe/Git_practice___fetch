928022a28ce84fcf5c0e94eb5f536e198d2e7118
Chocobe <kyw05171@gmail.com>
2019-01-15 00:13:43 +0900

    <master> : Modificated code in Local repository

--- sub1/Hello.java
+++ sub1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Tell your World");
+		System.out.println("Modificated code in Local repository");
 	}
 }
